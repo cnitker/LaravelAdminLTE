130477a67422df6bfbea32dcad0aabdeed51a9b8
Jack.Li <aisun86@gmail.com>
2019-04-12 11:16:24 +0800

    增加composer包文件

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    //
+    public function index(){
+//        $client =new Client([
+//            'base_url'=>'https://www.baidu.com',
+//            'timeout'=>2.0
+//        ]);
+//        $response = $client->request('get','');
+//        return $response;
+
+//        $client =new Client();
+//        $res = $client->request('GET', 'https://www.baidu.com', [
+//            'auth' => ['user', 'pass']
+//        ]);
+//
+////        echo $res->getStatusCode();
+//        echo $res->getBody();
+
+        $response = \Requests::get('http://www.baidu.com');
+
+        return $response->body;
+
+    }
+}
